fe8f8371730c115e46e03296da04b9d334a8ca37
Anton Komlev <anton.komlev@arm.com>
2026-04-09 15:03:14 +0100

    Build: Fix RTX build for M55+FPU
     - Use CMSIS device headers with _DSP_FP extension for M33 and M35P only
     - NSID manager as a separate library
     - Separate a platform default initialization file with weak symbols
    
    Change-Id: Iab128de57350d5fa8251924c3c479910a8940d0e
    Signed-off-by: Anton Komlev <anton.komlev@arm.com>

--- app_broker/main_ns.c
+++ app_broker/main_ns.c
@@ -1,7 +1,5 @@
 /*
  * SPDX-FileCopyrightText: Copyright The TrustedFirmware-M Contributors
- * Copyright (c) 2022 Cypress Semiconductor Corporation (an Infineon company)
- * or an affiliate of Cypress Semiconductor Corporation. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  *
@@ -15,12 +13,14 @@
 #include "tfm_plat_ns.h"
 #include "Driver_USART.h"
 #include "device_cfg.h"
+
 #ifdef TFM_NS_MAILBOX_API
 #include "tfm_multi_core_api.h"
 #include "tfm_ns_mailbox.h"
 #endif
 #include "test_log.h"
 #include "uart_stdout.h"
+
 #if (CONFIG_TFM_FLOAT_ABI >= 1)
 #include "cmsis.h"
 #endif
@@ -96,45 +96,6 @@ static void tfm_ns_multi_core_boot(void)
 #else /* TFM_NS_MAILBOX_API */
 extern uint32_t tfm_ns_interface_init(void);
 #endif /* TFM_NS_MAILBOX_API */
-
-/**
- * \brief Platform peripherals and devices initialization.
- *        Can be overridden for platform specific initialization.
- *
- * \return  ARM_DRIVER_OK if the initialization succeeds
- */
-__WEAK int32_t tfm_ns_platform_init(void)
-{
-    stdio_init();
-
-    return ARM_DRIVER_OK;
-}
-
-/**
- * \brief Platform peripherals and devices de-initialization.
- *        Can be overridden for platform specific initialization.
- *
- * \return  ARM_DRIVER_OK if the de-initialization succeeds
- */
-__WEAK int32_t tfm_ns_platform_uninit(void)
-{
-    stdio_uninit();
-
-    return ARM_DRIVER_OK;
-}
-
-
-__WEAK int32_t tfm_ns_cp_init(void)
-{
-#if (CONFIG_TFM_FLOAT_ABI >= 1)
-#ifdef __GNUC__
-    /* Enable NSPE privileged and unprivilged access to the FP Extension */
-    SCB->CPACR |= (3U << 10U*2U)     /* enable CP10 full access */
-                  | (3U << 11U*2U);  /* enable CP11 full access */
-#endif
-#endif
-    return ARM_DRIVER_OK;
-}
 
 /**
  * \brief main() function
---
+++ app_broker/platform_init_def.c
@@ -0,0 +1,49 @@
+/*
+ * SPDX-FileCopyrightText: Copyright The TrustedFirmware-M Contributors
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+#include "cmsis_compiler.h"
+#include "Driver_USART.h"
+#include "uart_stdout.h"
+
+ /**
+ * \brief Platform peripherals and devices initialization.
+ *        Can be overridden for platform specific initialization.
+ *
+ * \return  ARM_DRIVER_OK if the initialization succeeds
+ */
+__WEAK int32_t tfm_ns_platform_init(void)
+{
+    stdio_init();
+
+    return ARM_DRIVER_OK;
+}
+
+/**
+ * \brief Platform peripherals and devices de-initialization.
+ *        Can be overridden for platform specific initialization.
+ *
+ * \return  ARM_DRIVER_OK if the de-initialization succeeds
+ */
+__WEAK int32_t tfm_ns_platform_uninit(void)
+{
+    stdio_uninit();
+
+    return ARM_DRIVER_OK;
+}
+
+
+__WEAK int32_t tfm_ns_cp_init(void)
+{
+#if (CONFIG_TFM_FLOAT_ABI >= 1)
+#ifdef __GNUC__
+    /* Enable NSPE privileged and unprivilged access to the FP Extension */
+    SCB->CPACR |= (3U << 10U*2U)     /* enable CP10 full access */
+                  | (3U << 11U*2U);  /* enable CP11 full access */
+#endif
+#endif
+    return ARM_DRIVER_OK;
+}
